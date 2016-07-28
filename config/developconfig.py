6ce1343d4925050371fdd41cf5f6131af6096800
andyweijin <wei.jin@dalegames.con>
2016-07-28 17:44:20 +0800

    rm some file

--- config/developconfig.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'wei.jin'
-
-
-#SQL_CONNECT_STRING = 'Driver={SQL Server};Server=127.0.0.1;Database=logs_db160720;Uid=;Pwd=DLGames888'
-SQL_CONNECT_STRING = 'Driver={SQL Server};Server=127.0.0.1;Database=fmo_log_cu1;Uid=;Pwd=DLGames888'
